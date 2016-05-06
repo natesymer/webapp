df5c683be562bd62e320d174103de82610a642fd
Nathaniel Symer <nate@natesymer.com>
2016-05-06 14:30:29 -0400

    Improved CLI & scaffolding
    The CLI parser was simplified. First, the actual code was refactored
    for brevity and point-freedom. Second, the pid-file option and
    daemonize flag were combined into one single option called daemonize.
    This makes sense because it forces the user to adhere to good daemonizing
    practices.
    
    The next major improvement was the elimination of status/stop features
    in the CLI. Quite frankly, these are more effectively done with a shell.
    Not having to have code to do this makes the CLI significantly thinner. It
    also allowed me to remove a module with less-than-pretty code. Everybody wins!

--- README.md
+++ README.md
@@ -1,32 +1,13 @@
 # webapp - WAI web framework
 
-Webapp is a web framework that is designed to provide everything needed to define & deploy a web app.
+Webapp is a web framework that is designed to provide everything needed to define & deploy a web app. For how to use, see Haddock documentation. For an example, see `example.hs`.
 
-Basic example:
+Webapp provides a function called `webappMain` (as well as a series of other similarly named functions) that start the built-in webserver. Your web application's `main` function should include a call to one of them at the end.
 
-    module Main where
-    
-    import Web.App
-	import qualified Control.Monad.State.Class as S
-    
-    instance WebAppState Integer where
-      initState = return 0
-      destroyState st = do
-        putStr "Counted: "
-        print st
+# Using a webapp web app
 
-    main = webappMainIO' app "My Web App"
+Once you've written your web app, deploying is up to you. Webapp will probably work with services like Heroku or complicated load balancers, but it designed to be a standalone server (i.e. no need to run behind something like nginx).
 
-    app :: WebAppT Integer IO ()
-    app = do
-      get "/" $ do
-        addHeader "Content-Type" "text/plain"
-   		S.get >>= writeBody . show
-    
-      get "/add" $ do
-      	S.state (((),) . (+) 1)
-   		redirect "/"
-        
-      get "/subtract" $ do
-        S.get >>= S.put . ((-) 1)
-        redirect "/"
+Webapp works by first binding to an IPv4 TCP port, immediately after which resigning privileges†. Then it builds a `WAI` app from your `WebAppT` app, applies middleware, and runs `Warp`.
+
+† The effective GID & UID are set to match the real GID & UID.
--- Web/App/Main.hs
+++ Web/App/Main.hs
@@ -20,6 +20,8 @@ main = webappMain' app "My Application!"
 
 -}
 
+{-# OPTIONS -fno-warn-unused-do-bind #-}
+
 module Web.App.Main
 (
   webappMain,
@@ -35,34 +37,26 @@ import Web.App.RouteT (RouteResult)
 import Web.App.State
 import Web.App.HTTP
 import Web.App.Internal.IO
-import Web.App.Internal.Daemon
 import Web.App.Internal.TerminalSize
 
 import Control.Monad.IO.Class
 
 import Control.Applicative
 import Options.Applicative
-import System.Environment (getArgs)
 import System.Posix
+import System.Exit
 
 data Cmd
   = StartCommand {
-    _startCmdDaemonize :: Bool,
+    _startCmdDaemonize :: Maybe FilePath,
     _startCmdInsecure :: Bool,
     _startCmdPort :: Int,
     _startCmdHTTPSSLCert :: FilePath,
     _startCmdHTTPSSLKey :: FilePath,
     _startCmdOutputPath :: Maybe FilePath,
-    _startCmdErrorPath :: Maybe FilePath,
-    _startCmdPidPath :: FilePath
+    _startCmdErrorPath :: Maybe FilePath
   }
-  | StopCommand {
-    _stopCmdPidPath :: FilePath
-  }
-  | StatusCommand {
-    _statusCmdPidPath :: FilePath
-  }
-  
+
 -- |Like 'webappMainIO' without the CLI extension arguments.
 webappMainIO' :: (WebAppState s)
               => WebApp s IO -- ^ app to start
@@ -93,19 +87,25 @@ webappMain :: (WebAppState s, MonadIO m)
            => (m RouteResult -> IO RouteResult) -- ^ action to eval a monadic computation in @m@ in @IO@
            -> WebApp s m -- ^ app to start
            -> String -- ^ CLI title/description
-           -> Maybe (Parser a) -- ^ extra CLI parser (available under @util@ subcommand)
-           -> (a -> IO ()) -- ^ action to apply to parse result of 'utilParser'
+           -> Maybe (Parser a) -- ^ extra CLI parser, parsed after the built-in parser
+           -> (a -> IO ()) -- ^ action to apply the result of 'extraParser'
            -> IO ()
-webappMain runToIO app title utilParser utilf = getArgs >>= getCommandArgs utilParser title >>= processArgs
+webappMain runToIO app title extraParser extraf = parseArgs extraParser title >>= either extraf f
   where
-    processArgs (Right cmd) = f cmd
-    processArgs (Left utils) = utilf utils
-    f (StartCommand True i p c k o e pidPath) = daemonize pidPath $ start i p c k o e
-    f (StartCommand False i p c k o e _) = start i p c k o e
-    f (StopCommand pidPath) = daemonKill 4 pidPath
-    f (StatusCommand pidPath) = daemonRunning pidPath >>= putStrLn . showStatus
-    showStatus True = "running"
-    showStatus False = "stopped"
+    f (StartCommand Nothing i p c k o e) = start i p c k o e
+    f (StartCommand (Just pidFile) i p c k o e) = do
+      forkProcess $ do
+        createSession
+        forkProcess $ do
+          getProcessID >>= writeFile pidFile . show
+          redirectStdout $ Just "/dev/null"
+          redirectStderr $ Just "/dev/null"
+          redirectStdin $ Just "/dev/null"
+          closeFd stdInput -- close STDIN
+          installHandler sigHUP Ignore Nothing
+          start i p c k o e
+        exitImmediately ExitSuccess
+      exitImmediately ExitSuccess
     start insecure port cert key out err = do
       bindTCP port $ \sock -> do
         -- drop privileges after binding to a port
@@ -120,29 +120,19 @@ webappMain runToIO app title utilParser utilf = getArgs >>= getCommandArgs utilP
             serveFunc False = runSecure cert key
             serve sock = serveApp (serveFunc insecure sock) runToIO app port
 
-getCommandArgs :: Maybe (Parser a) -> String -> [String] -> IO (Either a Cmd)
-getCommandArgs utilParser title args = do
+parseArgs :: Maybe (Parser a) -> String -> IO (Either a Cmd)
+parseArgs extraParser title = do
   w <- maybe 80 snd <$> getTermSize
-  handleParseResult $ execParserPure (pprefs w) parser args
+  customExecParser (mkprefs w) parser
   where
-    pprefs = ParserPrefs "" False False True
-    parser = info (helper <*> ((sp utilParser) <|> parseStart)) (fullDesc <> header title)
-    sp Nothing = subparser subCommands
-    sp (Just util) = subparser $ subCommands <> (mkcmd "util" "Utilities associated with the application" (Left <$> util))
-    subCommands = (mkcmd "start" "Start the application server" parseStart) <>
-                  (mkcmd "stop" "Stop the application server" parseStop) <>
-                  (mkcmd "status" "Determine if the application server is running" parseStatus)
-    parseStart = fmap Right $ StartCommand
-      <$> (flag False True $ short 'd' <> long "daemonize" <> help "run the application server daemonized")
-      <*> (flag False True $ short 'i' <> long "insecure" <> help "run the application server over insecure HTTP")
-      <*> (option auto $ opt "port" 'p' "PORT" (Just 3000) "port to run the application server on")
-      <*> (strOption $ opt "https-crt" 'c' "FILEPATH" (Just "server.crt") "SSL certificate file")
-      <*> (strOption $ opt "https-key" 'k' "FILEPATH" (Just "server.key") "SSL private key file")
-      <*> (optional $ strOption $ opt "stdout" 'o' "FILEPATH" Nothing "redirect standard output to FILEPATH")
-      <*> (optional $ strOption $ opt "stderr" 'e' "FILEPATH" Nothing "redirect standard error to FILEPATH")
-      <*> (strOption $ opt "pid-file" 'z' "FILEPATH" (Just "/tmp/webapp.pid") "when daemonizing, write the PID to FILEPATH")
-    parseStop     = Right <$> StopCommand <$> (strOption $ opt "pid-file" 'z' "FILEPATH" (Just "/tmp/webapp.pid") "pid file")
-    parseStatus   = Right <$> StatusCommand <$> (strOption $ opt "pid-file" 'z' "FILEPATH" (Just "/tmp/webapp.pid") "pid file")
-    opt lng shrt mvar (Just defVal) hlp = (long lng <> short shrt <> metavar mvar <> value defVal <> help hlp)
-    opt lng shrt mvar Nothing       hlp = (long lng <> short shrt <> metavar mvar <> help hlp)
-    mkcmd cmd desc p = command cmd $ info (helper <*> p) $ progDesc desc
+    mkprefs = ParserPrefs "" False False True
+    mkparser = (<|>) (Right <$> parseStart) . maybe empty (fmap Left)
+    parseStart = StartCommand
+      <$> (optional $ strOption $ long "daemonize" <> short 'd' <> metavar "FILEPATH" <> help "daemonize server and write its pid to FILEPATH")
+      <*> (flag False True $ short 'i' <> long "insecure" <> help "run server over insecure HTTP")
+      <*> (option auto $ long "port" <> short 'p' <> metavar "PORT" <> value 3000 <> help "run server on PORT")
+      <*> (strOption $ long "cert" <> short 'c' <> metavar "FILEPATH" <> value "server.crt" <> help "SSL certificate file")
+      <*> (strOption $ long "key" <> short 'k' <> metavar "FILEPATH" <> value "server.key" <> help "SSL private key file")
+      <*> (optional $ strOption $ long "stdout" <> short 'o' <> metavar "FILEPATH" <> help "redirect output to FILEPATH")
+      <*> (optional $ strOption $ long "stderr" <> short 'e' <> metavar "FILEPATH" <> help "redirect error to FILEPATH")
+    parser = info (helper <*> mkparser extraParser) (fullDesc <> header title)
